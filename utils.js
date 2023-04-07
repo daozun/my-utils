128ec9dda05d62b7281ca9f4877be24cf3ef2bdf
daozun <1836643373@qq.com>
2023-04-07 13:23:17 +0800

    Update utils.js
    添加工具函数（convertToFormData）：把 json 数据转成 FormData 格式

--- utils.js
+++ utils.js
@@ -29,3 +29,37 @@ function dropDuplicate(arr) {
 
   return uniqueArray;
 }
+
+// 把 json 数据转成 FormData 格式
+function convertToFormData(data) {
+  let formData = new FormData();
+
+  // 递归处理对象属性
+  function appendFormData(formData, data, parentKey) {
+    if (Array.isArray(data)) {
+      // 如果是数组，处理数组元素
+      for (let i = 0; i < data.length; i++) {
+        appendFormData(formData, data[i], parentKey + "[" + i + "]");
+      }
+    } else if (typeof data === "object" && data !== null) {
+      // 如果是对象，递归处理对象属性
+      for (let key in data) {
+        // eslint-disable-next-line no-prototype-builtins
+        if (data.hasOwnProperty(key)) {
+          if (parentKey) {
+            appendFormData(formData, data[key], parentKey + "." + key);
+          } else {
+            appendFormData(formData, data[key], key);
+          }
+        }
+      }
+    } else {
+      // 否则，直接添加属性到 FormData
+      formData.append(parentKey, data);
+    }
+  }
+
+  appendFormData(formData, data, "");
+
+  return formData;
+}
